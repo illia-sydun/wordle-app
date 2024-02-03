df08f2dee11fac5626146e7cc209a3eed72aadd9
Illia Sydun <illia@memcare.com>
2024-02-03 18:08:37 -0500

    Install prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,21 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    "plugin:react/recommended",
     'plugin:react-hooks/recommended',
+    "plugin:prettier/recommended"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-hooks', 'react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "react/react-in-jsx-scope": "off",
   },
 }
