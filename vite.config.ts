08d1ce3c1c9d367aa153d3ffc23b123df2869155
Illia Sydun <illia.sydun@gmail.com>
2024-02-10 20:52:07 -0500

    Setup TypeScript plugin and add a few aliases to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
+
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), checker({ typescript: true })],
     resolve: {
         alias: {
-            '@styles': path.resolve(__dirname, './src/styles/base'),
+            '@styles': path.resolve('./src/shared/styles/base'),
+            '@shared': path.resolve('./src/shared'),
         },
     },
 });
