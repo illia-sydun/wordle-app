5a2f9c35b58a2e432dcb23bdd3f06f94904075b0
Illia Sydun <illia@memcare.com>
2024-03-18 14:55:52 -0400

    Improve typing of shared constants

--- src/shared/constants/CellStatus.ts
+++ src/shared/constants/CellStatus.ts
@@ -2,10 +2,10 @@ export const CELL_STATUS_SUBMITTED = {
     NO_MATCH: 'no_match',
     CLOSE_MATCH: 'close_match',
     MATCH: 'match',
-} as const;
+} as const satisfies Record<string, string>;
 
 export const CELL_STATUS = {
+    ...CELL_STATUS_SUBMITTED,
     EMPTY: 'empty',
     TOUCHED: 'touched',
-    ...CELL_STATUS_SUBMITTED,
-} as const;
+} as const satisfies Record<string, string>;
