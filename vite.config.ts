cfaafcd80d6ade0c2072db0270eae666ae87612c
Illia Sydun <illia.sydun@gmail.com>
2024-03-21 16:14:21 -0400

    Introduce PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,53 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
 import path from 'path';
+
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+    registerType: 'autoUpdate',
+    strategies: 'generateSW',
+    workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,woff2}'],
+    },
+    manifest: {
+        name: 'Wordle',
+        short_name: 'Wordle',
+        description: 'Wordle',
+        icons: [
+            {
+                src: '/android-chrome-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+                purpose: 'favicon',
+            },
+            {
+                src: '/android-chrome-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'favicon',
+            },
+            {
+                src: '/apple-touch-icon.png',
+                sizes: '180x180',
+                type: 'image/png',
+                purpose: 'apple touch icon',
+            },
+            {
+                src: '/maskable_icon.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any maskable',
+            },
+        ],
+        background_color: '#222222',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        orientation: 'portrait',
+    },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +65,7 @@ export default defineConfig({
             },
         }),
         checker({ typescript: true }),
+        VitePWA(manifestForPlugIn),
     ],
     resolve: {
         alias: {
