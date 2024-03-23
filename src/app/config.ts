40d93f11bc80d967c3bb3b7ad378470e214ad33e
Illia Sydun <illia.sydun@gmail.com>
2024-03-22 21:19:40 -0400

    Add sentry and zod for .env variables type safety

---
+++ src/app/config.ts
@@ -0,0 +1,20 @@
+import * as Sentry from '@sentry/react';
+import '../zod-env.ts';
+
+Sentry.init({
+    dsn: 'https://67d8d6828625678362aa5e7abf9320a3@o4506954991075328.ingest.us.sentry.io/4506954993827840',
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration({
+            maskAllText: false,
+            blockAllMedia: false,
+        }),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
---
+++ src/vite-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_APP_NAME: string;
+    readonly SENTRY_AUTH_TOKEN: string;
+}
